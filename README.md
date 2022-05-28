622cbd5d164d26f0bdd33c12c5ec8e6eb668d4e6
Trevor Bergeron <mal@sec.gd>
2022-05-27 21:46:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# sopel-http-example
+[![PyPi Version](https://img.shields.io/pypi/v/sopel-http-example.svg)](https://pypi.org/project/sopel-http-example/)
+
+An example plugin for [sopel-http](https://github.com/half-duplex/sopel-http)
+
+## Setup
+Just `pip install sopel-http-example` into your
+[Sopel](https://github.com/sopel-irc/sopel) environment.
+
+## Usage
+Once this plugin is installed and your bot is restarted, you should be able to
+access the web UI on https://localhost:8094/ (or whatever host/port you
+configured `sopel-http` to use).
---
+++ sopel_http_example/__init__.py
@@ -0,0 +1,65 @@
+"""sopel-http-example
+
+https://github.com/half-duplex/sopel-http-example
+Released under the EUPL-1.2
+"""
+
+from __future__ import annotations
+
+from os import path
+
+from flask import abort, Blueprint, render_template, request, url_for
+from markupsafe import escape
+import sopel_http
+
+module_dir = path.dirname(__file__)
+app = Blueprint(
+    "example",
+    __name__,
+    static_folder=path.join(module_dir, "static"),
+    static_url_path="/static/example",
+    template_folder=path.join(module_dir, "templates"),
+)
+bot = None
+
+
+def setup(newbot):
+    global bot
+    bot = newbot
+    sopel_http.app.register_blueprint(app)
+
+
+@app.route("/")
+def hello():
+    message = "Hello!\n"
+    message += "I am in the following channels:\n"
+    for channel in bot.channels.keys():
+        message += """<a href="{channel_link}">{channel}</a>\n""".format(
+            channel_link=url_for("example.channel", channel=channel),
+            channel=escape(channel),
+        )
+    return message.replace("\n", "<br>\n")
+
+
+@app.route("/channel/<channel>")
+def channel(channel):
+    if channel not in bot.channels:
+        abort(404)
+    return render_template(
+        "channel.j2",
+        channel=channel,
+        users=bot.channels[channel].privileges.keys(),
+    )
+
+
+@app.route("/channel/<channel>", methods=["POST"])
+def channel_post(channel):
+    if channel not in bot.channels:
+        abort(404)
+    if "user" not in request.form:
+        abort(400, "User not specified")
+    user = request.form["user"]
+    if request.form["user"] not in bot.channels[channel].privileges:
+        abort(400, "User not specified")
+    bot.say("A web user says hello, " + user + "!", channel)
+    return "Sent!"
